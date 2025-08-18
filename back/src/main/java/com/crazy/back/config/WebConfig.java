0c85cbfdb627ce59ca5ced3c8e56e55aba9ace8b
jose-manuel-byte <papepillo12345@gmail.com>
2025-08-18 22:04:40 +0200

    Enable data web support for serializing pageimpl as a DTO

--- back/src/main/java/com/crazy/back/config/WebConfig.java
+++ back/src/main/java/com/crazy/back/config/WebConfig.java
@@ -4,8 +4,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode;
 
 @Configuration
+@EnableSpringDataWebSupport(pageSerializationMode = PageSerializationMode.VIA_DTO)
 public class WebConfig {
 
     @Bean
